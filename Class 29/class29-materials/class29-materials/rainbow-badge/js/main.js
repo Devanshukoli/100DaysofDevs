73d77eaeb6f3b4bef1938f10ef00530336fdd3cb
Devanshukoli <kolidevanshu02@gmail.com>
2025-03-10 09:06:47 +0530

    class 29
    just messing with ranibow-badge

--- Class 29/class29-materials/class29-materials/rainbow-badge/js/main.js
+++ Class 29/class29-materials/class29-materials/rainbow-badge/js/main.js
@@ -1,45 +1,44 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
+function getFetch() {
   const poke1 = document.querySelector('#poke1').value
   const poke2 = document.querySelector('#poke2').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+poke1
-  const url2 = 'https://pokeapi.co/api/v2/pokemon/'+poke2
+  const url = `https://pokeapi.co/api/v2/pokemon/${poke1}`
+  const url2 = `https://pokeapi.co/api/v2/pokemon/${poke2}`
+  console.log(url)
+  console.log(url2)
+
   let pokeStore = []
   let pokeImg = []
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      pokeStore.push(data.types[0].type.name)
+      pokeImg.push(data.sprites.front_shiny)
 
-        pokeStore.push(data.types[0].type.name)
-        pokeImg.push(data.sprites.front_shiny)
-        
-        fetch(url2)
+      fetch(url2)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
 
           pokeStore.push(data.types[0].type.name)
           pokeImg.push(data.sprites.front_shiny)
-      
-          if((pokeStore[0] === "grass" && pokeStore[1] === 'water')){
+
+          if ((pokeStore[0] === "grass" && pokeStore[1] === 'water')) {
             document.querySelector('#pokeImg1').src = pokeImg[0]
             document.querySelector('#pokeImg2').src = pokeImg[1]
             document.querySelector('h2').innerText = " 2x > "
           }
         })
         .catch(err => {
-            console.log(`error ${err}`)
+          console.log(`error in second.... ${err}`)
         });
 
 
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    })
+    .catch(err => {
+      console.log(`error in first... ${err}`)
+    });
 
-
-
-      
 }
