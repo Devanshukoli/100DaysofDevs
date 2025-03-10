024d04d7857c6f3b9cd368fa61edc9df8cfab022
Devanshukoli <kolidevanshu02@gmail.com>
2025-03-10 09:24:00 +0530

    class 29
    just completing thunder-badge.

--- Class 29/class29-materials/class29-materials/thunder-badge/js/main.js
+++ Class 29/class29-materials/class29-materials/thunder-badge/js/main.js
@@ -1,2 +1,41 @@
 //Objects
 //Please create a pokemon class with as much detail as possible in 10 minutes
+
+class Pokemon {
+    constructor(name, type, level = 1) {
+        this.name = name
+        this.type = type
+        this.level = level
+        this.health = 100
+        this.attack = 50
+        this.defense = 20
+        this.moves = []
+        this.expereince = 10
+        this.isShiny = Math.random() < 0.1 
+    }
+
+    levelUp() {
+        this.level++
+        this.health += 5
+        this.attack += 3
+        this.defense += 4
+    }
+
+    learnsMove() {
+        if (this.moves.length < 4) {
+            this.moves.push(move)
+        } else {
+            console.log("Pokemon can only have 4 moves")
+        }
+    }
+
+    attack(opponent) {
+        const damage = Math.floor(this.attack * (1 - opponent.defense / 100))
+        opponent.health -= damage
+        return damage
+    }
+}
+
+const pikachu = new Pokemon('Pikachu', "electric", 5)
+pikachu.learnsMove('Thunderbolt')
+pikachu.learnsMove('Quick attack')
